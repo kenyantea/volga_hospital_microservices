6417ebf256140000559251d6e6550ff0be73a515
kenyantea <kenyantea@users.noreply.github.com>
2024-09-17 00:04:58 +0300

    инициализация репозитория

---
+++ hospitals/src/test/java/com/example/hospitals/HospitalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hospitals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
